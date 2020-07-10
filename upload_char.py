94152b8e105f4a108be8bdaba1f6dc19a11e8117
Cami-Jahr <joachimjahr@hotmail.com>
2020-07-10 17:35:06 +0200

    added other pages

--- upload_char.py
+++ upload_char.py
@@ -11,6 +11,8 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from helpers import get_filenames, get_char_list
+from pathlib import Path
+import os
 
 class Uploader:
     def __init__(self):
@@ -99,6 +101,63 @@ class Uploader:
         self.upload("/".join([char, "Abilities"]), text)
 
 
+def trivia_section(char):
+    text = "{{SideStory\n|Side Story Translation Link = \n|Side Story = \n}}\n\n==Trivia==\n*\n\n{{Videos}}"
+    return text
+    #self.upload("/".join([char, "Trivia"]), text)
+
+def voice_section(char, text):
+    return text
+    #self.upload("/".join([char, "Quotes"]), text)
+
+def upgrades_section(char, text):
+    stats = "{{" + """{} Items|Items""".format(char.replace("_", " ")) + "}}"
+    #self.upload("Template:{}_Items".format(char), text)
+    #self.upload("{}/Upgrades".format(char), stats)
+    return text, stats
+
+def main_section(char, stats, text):
+    text = "{{" + char + "}}" + "\n\n{{Description\n| en = \n| na = \n| jp = " + text + "\n}}\n\n{{Tabs}}"
+    #self.upload("Template:" + char, stats)
+    #self.upload(char, )
+    return stats, text
+
+def abilities_section(char, doppel_story):
+    text = "{{" + char + "|Abilities}}"
+    if doppel_story:
+        text += "\n\n{{Description\n| en = \n| na = \n| jp = " + doppel_story + "\n}}"
+    return text
+    #self.upload("/".join([char, "Abilities"]), text)
+
+galley_base = """<tabber>
+Memoria=
+==Personal [[Memoria]]==
+{| class="wikitable" style="width:100%"
+| style="width:50%" |
+| style="width:50%" |
+|}
+==4✵ Memoria==
+{| class="wikitable" style="width:100%"
+| style="width:50%" |
+| style="width:50%" |
+|}
+
+|-|Videos=
+{{Videos}}
+|-|CG=
+==Magical Girl Stories==
+<gallery position="center" columns="3">
+
+</gallery>
+</tabber>
+"""
+
+costume_base = """<tabber>
+Magical Girl = 
+</tabber>
+"""
+
+
 if __name__ == '__main__':
     #S = Uploader()
     #S.login(username, password)
@@ -115,12 +174,34 @@ if __name__ == '__main__':
     formated_trivia = story.story_formater(trivia)
 
     for i in list(chars):
-        s = chars[i].replace(" ", "_")
-        print(i, chars[i], f"https://magireco.fandom.com/wiki/Template:{s}?action=edit")
-        out = read_stats.format_info(i)
-        #print(out)
-        with open(f"stats/{s}.txt", "w", encoding="utf-8-sig") as f:
-            f.write(out)
+        ch = chars[i].replace(" ", "_")
+        print(ch)
+        parent = os.path.join("wikia_pages", ch)
+        Path(parent).mkdir(parents=True, exist_ok=True)
+        #print(i, chars[i], f"https://magireco.fandom.com/wiki/Template:{ch}?action=edit")
+
+        main_temp_text, main_page_text = main_section(ch, read_stats.format_info(i), formated_trivia[i][0])
+        ability_page_text = abilities_section(ch, formated_trivia[i][1])
+        upgrade_temp_text, upgrade_page_text = upgrades_section(ch, formated_mats[i])
+        trivia_text = trivia_section(ch)
+        voice_page_text = voice_section(ch, formated_voice[i])
+
+
+        for page, text in (
+            (f"Template-{ch}", main_temp_text),
+            (f"Template-{ch}_Items", upgrade_temp_text),
+            (f"{ch}", main_page_text),
+            (f"{ch}-Abilities", ability_page_text),
+            (f"{ch}-Upgrades", upgrade_page_text),
+            (f"{ch}-Trivia", trivia_text),            
+            (f"{ch}-Costumes", costume_base),            
+            (f"{ch}-Gallery", galley_base),
+            (f"{ch}-Quotes", voice_page_text)):
+            with open(os.path.join(parent, page + ".txt"), "w", encoding="utf-8-sig") as f:
+                f.write(text)
+
+        with open(f"stats/{ch}.txt", "w", encoding="utf-8-sig") as f:
+            f.write(main_temp_text)
     """
     for _id in up_chars:
         S.main_section(chars[_id], read_stats.format_info(_id), formated_trivia[_id][0])
