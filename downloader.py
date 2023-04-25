13148998adf8cef14df4f8f706bd5a4b76a3ff41
Cami-Jahr <joachimjahr@hotmail.com>
2023-04-25 18:14:51 +0200

    Parallelized downloader and removed print spam

--- downloader.py
+++ downloader.py
@@ -1,13 +1,14 @@
+import concurrent.futures
 import os
 import ssl
 import urllib.request as ur
 from json import (
     dumps,
     loads)
-from multiprocessing import Process
 from subprocess import (
-    PIPE,
-    Popen)
+    DEVNULL,
+    Popen,
+    STDOUT)
 
 ssl._create_default_https_context = ssl._create_unverified_context
 
@@ -18,39 +19,23 @@ online_path_en = "https://android.magica-us.com/magica/resource/download/asset/m
 temp_path = "D:/MagirecoTemps/"
 
 
-def download_files(json, old_files, file_name, savepoint, online_path, local_path, complete_json):
-    old_path = ""
-    done = []
-    length = len(json)
-    nr = 0
-    show = 0
-    print(f"Total size of {file_name} is {length}. Savepoint sat to {savepoint}. Already have {len(old_files)} files")
-    for item in json:
-        nr += 1
-        show += 1
+def download_files(item, old_files, online_path, local_path):
+    try:
         file = item["path"]
         file_list = item["file_list"]
         try:
             if old_files[file] == item["md5"]:
-                done.append(item)
-                if show == savepoint:
-                    show = 0
-                    print("{:.2f}% done with {}".format(nr / length * 100, file_name))
-                    with open("download_tracking/" + file_name + ".json", "w") as f:
-                        f.write(dumps(done))
-                continue
+                return item
             else:
                 print("E", end="")
         except KeyError:  # item not in old_files json
             pass
-        print("STARTING_FILE: {:<75}".format(file), end="", flush=True)
+        # print("STARTING_FILE: {:<75}".format(file))
 
         path = "/".join(file.split("/")[:-1]) + "/"
-        if old_path != path:  # New path
-            old_path = path
-            if not os.path.exists(local_path + old_path):
-                os.makedirs(local_path + old_path)
-                print("CREATED_DICT: " + local_path + old_path)
+        if not os.path.exists(local_path + path):
+            os.makedirs(local_path + path)
+            # print("CREATED_DICT: " + local_path + path)
 
         if len(file_list) > 1:
             paths = []
@@ -70,8 +55,8 @@ def download_files(json, old_files, file_name, savepoint, online_path, local_pat
             if _type == "png" or _type == "jpg":
                 Popen(
                     ['ffmpeg', "-y", "-i", 'concat:{}'.format("|".join(paths)), "-c", "copy", local_path + file],
-                    stdout=PIPE,
-                    stdin=PIPE)
+                    stdout=DEVNULL,
+                    stderr=STDOUT)
             else:
                 _object = b""
                 for path in paths:
@@ -83,21 +68,12 @@ def download_files(json, old_files, file_name, savepoint, online_path, local_pat
             with open(local_path + file, "wb") as f:
                 request = ur.Request(online_path + file_list[0]['url'])
                 f.write(ur.urlopen(request).read())
-        print("CREATED_FILE: " + file, flush=True)
-
-        done.append(item)
-        if show == savepoint:
-            show = 0
-            print("{:.2f}% done with {}".format(nr / length * 100, file_name))
-            with open("download_tracking/" + file_name + ".json", "w") as f:
-                f.write(dumps(complete_json + done))
-    with open("download_tracking/" + file_name + ".json", "w") as f:
-        f.write(dumps(done))
-    with open("download_tracking/" + file_name + "_full.json", "w") as f:
-        f.write(dumps(done))
+        return item
+    except Exception as e:
+        return e
 
 
-def run(file_name, path, nr, online_path, local_path):
+def run(file_name, path, savepoint, online_path, local_path):
     try:
         with open("download_tracking/" + file_name + ".json", "r") as f:
             ongoing_json = loads(f.read())
@@ -114,8 +90,29 @@ def run(file_name, path, nr, online_path, local_path):
         old_files[_item["path"]] = _item["md5"]
 
     _request = ur.Request(path)
-    online_json = loads(ur.urlopen(_request).read().decode("utf-8"))
-    download_files(online_json, old_files, file_name, nr, online_path, local_path, complete_json)
+    json = loads(ur.urlopen(_request).read().decode("utf-8"))
+    done = []
+    length = len(json)
+    nr = 0
+    print(f"Total size of {file_name} is {length}. Savepoint sat to {savepoint}. Already have {len(old_files)} files")
+    with concurrent.futures.ThreadPoolExecutor(max_workers=20) as executor:
+        for items in [json[x:x + savepoint] for x in range(0, len(json), savepoint)]:
+            nr += savepoint
+            future_to_item = {executor.submit(download_files, item, old_files, online_path, local_path): item for item in items}
+            for future in concurrent.futures.as_completed(future_to_item):
+                item = future_to_item[future]
+                if item is Exception:
+                    raise item
+                else:
+                    done.append(item)
+            print("{:.2f}% done with {}".format(min(100.0, nr / length * 100), file_name))
+            with open("download_tracking/" + file_name + ".json", "w") as f:
+                f.write(dumps(done))
+
+    with open("download_tracking/" + file_name + ".json", "w") as f:
+        f.write(dumps(done))
+    with open("download_tracking/" + file_name + "_full.json", "w") as f:
+        f.write(dumps(done))
     print("\nJSON_IS_DONE: " + file_name + "\n")
 
 
@@ -136,11 +133,11 @@ def run_en(file_name, source, nr):
 
 
 if __name__ == '__main__':
-    Process(target=run_jp, args=("files_main", "asset_main", 25)).start()
-    # Process(target=run_jp, args=("files_voice", "asset_voice", 40)).start()
-    # Process(target=run_jp, args=("files_prologue_voice", "asset_prologue_voice", 40)).start()
-    Process(target=run_jp, args=("files_prologue_main", "asset_prologue_main", 40)).start()
-    Process(target=run_jp, args=("files_char_list", "asset_char_list", 40)).start()
-    # Process(target=run_jp, args=("files_fullvoice", "asset_fullvoice", 40)).start()
-    Process(target=run_jp, args=("files_movie_high", "asset_movieall_high", 5)).start()
-    # Process(target=run_en, args=("files_movie_high_en", "asset_movie_high", 5)).start()
+    run_jp("files_prologue_voice", "asset_prologue_voice", 40)
+    run_jp("files_prologue_main", "asset_prologue_main", 200)
+    run_jp("files_movie_high", "asset_movieall_high", 20)
+    run_jp("files_char_list", "asset_char_list", 200)
+    run_jp("files_main", "asset_main", 25)
+    # run_jp("files_voice", "asset_voice", 100)
+    # run_jp("files_fullvoice", "asset_fullvoice", 100)
+    # run_en("files_movie_high_en", "asset_movie_high", 50)
