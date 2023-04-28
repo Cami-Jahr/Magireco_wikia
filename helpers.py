e9fbad30d1c293252b09f5d3bc15edf5d19db595
Cami-Jahr <joachimjahr@hotmail.com>
2023-04-28 18:29:18 +0200

    reverted helpers.py changes

--- helpers.py
+++ helpers.py
@@ -1,14 +1,10 @@
-import requests
-import re
-
-
 def get_char_list():
-    di = get_online_list("User:Thefrozenfish/Sandbox/CharacterListing")
+    di = get_list("txts/chars.txt")
     return di
 
 
 def get_memo_list():
-    return get_online_list("User:Thefrozenfish/Sandbox/MemoriaListing")
+    return get_list("txts/memoria_url_id.txt")
 
 
 def get_filenames():
@@ -22,14 +18,4 @@ def get_list(li):
             if line.strip():
                 _id, name = line.strip().split(";")
                 names[int(_id)] = name
-    return names
-
-
-def get_online_list(li):
-    names = {}
-    text = requests.get('https://magireco.fandom.com/wiki/' + li).text
-    for line in re.search(r'.*?<div class="mw-parser-output"><p>(.*?)</p>.*?', text, re.DOTALL).group(1).split("¤"):
-        if line.strip():
-            _id, name = line.strip().replace("&amp;", "&").split(";")
-            names[int(_id)] = name
     return names
